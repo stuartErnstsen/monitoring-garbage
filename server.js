6582fb8eb90d3707860cfcdadba96ef4faa9f7c4
stuartErnstsen <stuarternstsen@gmail.com>
2021-09-27 21:38:24 -0600

    rollbar log in add student endpoint

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/api/students', (req, res) => {
     students.push(name)
 
 
-
+    rollbar.log('Student added successfully', { author: 'Stuart' })
     res.status(200).send(students)
 })
 
